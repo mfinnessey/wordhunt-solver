987162c719d685390d3474c697283e269a28ef85
Michael Finnessey <mrfinnessey@gmail.com>
2025-02-09 00:03:41 -0800

    initial testing of board scoring
    fixed bugs:
    - initial traversals were ending before the final tile (bottom right)
    - duplicate words were scoring
    
    added test case from real game and verified words and total score

--- src/board.rs
+++ src/board.rs
@@ -6,6 +6,9 @@ use trie_rs::Trie;
 
 /// a concrete board. (0,0) is the top left corner and (3, 3) is the bottom right corner.
 pub struct Board<'a> {
+    /// tiles are read from left to right, top to bottom
+    /// 0 1 2 3
+    /// 4 5 6 7 ...
     tiles: [Letter; TILE_COUNT],
     score: u32,
     dictionary: &'a Trie<Letter>,
@@ -32,11 +35,10 @@ impl<'a> Board<'a> {
     }
 
     /// compute the maximum score for a given board
-    // TODO unit test this - correctness issue - use scores from official word hunts
     pub fn maximum_score(&mut self) -> &u32 {
         // push the state onto the search stack (dfs for theoretical locality advantage)
         // for each starting tile
-        for position in 0..15 {
+        for position in 0..TILE_COUNT {
             let visited_mask: u16 = 1 << position;
             let mut search = self.dictionary.inc_search();
             search.query(&self.tiles[position]);
@@ -113,24 +115,26 @@ impl<'a> Board<'a> {
         }
 
         // extend the search with the new letter
-
         let mut new_search = search.clone();
         let postfixes_exist;
         let tile = self.tiles[candidate_position as usize].clone();
         match new_search.query(&tile) {
-            Some(Answer::PrefixAndMatch) | Some(Answer::Match) => {
-                postfixes_exist = true;
-
-                // only valid words of length at least 3 score
-                if new_search.prefix_len() >= 3 {
-                    self.score += POINTS[new_search.prefix_len()];
-                    self.words.insert(new_search.prefix());
-                }
-            }
-
             Some(Answer::Prefix) => {
                 postfixes_exist = true;
             }
+
+            Some(Answer::PrefixAndMatch) => {
+                postfixes_exist = true;
+
+                self.score_word(new_search.prefix());
+            }
+
+            Some(Answer::Match) => {
+                postfixes_exist = false;
+
+                self.score_word(new_search.prefix());
+            }
+
             None => {
                 postfixes_exist = false;
             }
@@ -143,8 +147,15 @@ impl<'a> Board<'a> {
         }
     }
 
-    pub fn get_words(&self) -> Vec<&Vec<Letter>> {
-        self.words.iter().collect()
+    #[inline(always)]
+    /// score points for the given word, accounting for duplicate words
+    fn score_word(&mut self, word: Vec<Letter>) {
+        let word_len = word.len();
+
+        // a word can only score once per board
+        if self.words.insert(word) {
+            self.score += POINTS[word_len];
+        }
     }
 }
 
@@ -160,4 +171,81 @@ fn get_xy(position: &u8) -> (u8, u8) {
 #[inline(always)]
 fn get_position(x: &u8, y: &u8) -> u8 {
     (y * 4) + x
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::letter::translate_word;
+    use crate::utilities::create_trie;
+    use std::collections::HashMap;
+
+    #[test]
+    fn test_maximum_score() {
+        // test design is to test against a real boards (and solutions) from a high-scoring
+        // game to cover a variety of traversal scenarios.
+        let word_list = create_trie("wordlist-actual.txt").0;
+        // F L O S
+        // E I M T
+        // R K E I
+        // O Y R A
+        const BOARD: &str = "FLOSEIMTRKEIOYRA";
+        let tiles = translate_word(BOARD).unwrap();
+        let mut board = Board::new(tiles.try_into().unwrap(), &word_list);
+
+        // manually verified that:
+        // - a word begins and ends (except ending at the bottom left tile) at every tile in this board
+        // - board contains duplicate words
+        // - board contains words of length [3, 8]
+        // - board contains words using connections in all directions
+        const SOLUTION_WORDS: [&str; 287] = [
+            "SMIRKIER", "STEMLIKE", "FILMIER", "LIMITER", "MIRKIER", "OSTIARY", "REFILMS",
+            "SMIRKER", "ELMIER", "ETOILE", "FIKERY", "FILMER", "KELIMS", "KRAITS", "LIMIER",
+            "LIMITS", "MERITS", "MIRKER", "MOILER", "MOTIER", "OILERY", "ORIOLE", "REFILM",
+            "RELIER", "REMITS", "RIMIER", "RIOTER", "ROKIER", "SMEARY", "SMILER", "SMIRKY",
+            "SMITER", "SMOILE", "SOMITE", "STIMIE", "TOILER", "YORKER", "YORKIE", "AIERY", "AIMER",
+            "ARETS", "EMITS", "EYRIE", "FIERY", "FILER", "FILMI", "FILMS", "FILOS", "FLIER",
+            "FLOTE", "ITEMS", "KEFIR", "KELIM", "KETOL", "KILOS", "KRAIT", "KYRIE", "LIFER",
+            "LIKER", "LIMEY", "LIMIT", "LIMOS", "MERIT", "METOL", "MIKRA", "MILER", "MILOS",
+            "MIRKY", "MITER", "MOIRE", "MOSTE", "MOTEY", "OILER", "OMITS", "ORIEL", "OSTIA",
+            "RAITS", "REKEY", "RELIE", "REMIT", "RETIA", "RIEMS", "RIFLE", "RIMER", "RIOTS",
+            "ROKER", "SMEAR", "SMERK", "SMILE", "SMIRK", "SMITE", "SMOTE", "SOLEI", "SOLER",
+            "STEAR", "STEIL", "STIME", "STIRE", "STIRK", "STOLE", "TEARY", "TERAI", "TIMER",
+            "TOILE", "TOMIA", "YOKEL", "AERY", "AIMS", "AIRY", "AITS", "ARET", "ELMS", "EMIR",
+            "EMIT", "EMOS", "EYRA", "EYRE", "FIKE", "FIKY", "FILE", "FILM", "FILO", "FIRE", "FIRK",
+            "FLIR", "ITEM", "KEIR", "KETO", "KETS", "KIEF", "KIER", "KILO", "KORE", "KRAI", "LEIR",
+            "LEKE", "LIEF", "LIER", "LIFE", "LIKE", "LIME", "LIMO", "LIRE", "LIRK", "LOIR", "LOME",
+            "LOST", "LOTE", "LOTI", "LOTS", "MERI", "MERK", "METS", "MIKE", "MILE", "MILO", "MIRE",
+            "MIRK", "MIRY", "MITE", "MOIL", "MOLE", "MOST", "MOTE", "MOTS", "OKRA", "OLMS", "OMER",
+            "OMIT", "OYER", "RAIT", "REIF", "REIK", "REKE", "REMS", "RETS", "RIEL", "RIEM", "RIFE",
+            "RILE", "RIME", "RIMS", "RIOT", "RITE", "RITS", "ROKE", "ROKY", "RYKE", "SMIR", "SMIT",
+            "SOIL", "SOLE", "SOLI", "SOME", "STEM", "STEY", "STIE", "STIR", "TEAR", "TEIL", "TEMS",
+            "TIAR", "TIER", "TIME", "TIRE", "TOIL", "TOLE", "TOME", "TOMS", "YEAR", "YERK", "YETI",
+            "YOKE", "YORE", "YORK", "AIM", "AIR", "AIT", "ARE", "ARK", "ARY", "EAR", "EIK", "EKE",
+            "ELF", "ELM", "EMO", "EMS", "ERA", "ERK", "FER", "FIE", "FIL", "FIR", "IOS", "IRE",
+            "IRK", "ITS", "KEA", "KEF", "KET", "KEY", "KIF", "KIR", "KOR", "KYE", "LEI", "LEK",
+            "LIE", "LOS", "LOT", "MET", "MIL", "MIR", "MOI", "MOL", "MOS", "MOT", "OIK", "OIL",
+            "OKE", "OLE", "OLM", "OMS", "ORE", "OYE", "RAI", "REF", "REI", "REM", "RET", "RIA",
+            "RIF", "RIM", "RIT", "ROK", "RYE", "SOL", "SOM", "SOT", "TEA", "TIE", "TOM", "YEA",
+            "YER", "YET", "YOK",
+        ];
+
+        let expected_score: u32 = SOLUTION_WORDS
+            .iter()
+            .map(|word| POINTS[word.chars().count()])
+            .sum();
+        let expected_words = HashSet::from_iter(
+            SOLUTION_WORDS
+                .iter()
+                .map(|word| translate_word(word).unwrap()),
+        );
+
+        let actual_score = board.maximum_score();
+
+        // check that we found all words and that the score is as expected
+        assert_eq!(&expected_score, actual_score);
+        assert_eq!(expected_words.symmetric_difference(&board.words).count(), 0);
+
+        // TODO test words of length [9, 15]
+    }
 }
