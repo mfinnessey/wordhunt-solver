f3a994ce26d64c2eb4c4f91e241e936042b0e755
Michael Finnessey <mrfinnessey@gmail.com>
2025-02-26 19:17:23 -0800

    tune search to more reasonable values

--- src/main.rs
+++ src/main.rs
@@ -33,10 +33,10 @@ fn main() {
         &trie,
         all_a_combination,
         get_combination_score,
-        164000,
+        1640,
         std::thread::available_parallelism().unwrap().get(),
     );
-    combination_evaluator.begin_combination_evaluation(10);
+    combination_evaluator.begin_combination_evaluation(120);
 
     // temporary test grid from https://www.youtube.com/watch?v=3cgr_GgA5ns
     /*    let test_board_chars = ['M', 'H', 'O', 'N', 'I', 'T', 'E', 'R', 'L', 'A', 'S', 'N', 'S', 'E', 'R', 'U'];
